2f446b7d581714c8287f784435fa1b5cc5b43925
sanket <sanketst@gmail.com>
2019-07-08 14:37:26 +0530

    Adding libxsmm-style tiled conv2d

--- apps/conv2d.c
+++ apps/conv2d.c
@@ -67,42 +67,54 @@ void naive_conv_fp_stride_1(
 }
 
 
-#define T_ofm_tile 16
-#define T_ifm_tile 16
-#define T_oj 16
-#define T_oi 16
-
-
-
-void padded_conv_fp_stride_1_tiled_core(int nImg, int nIfm, int nOfm, int ifhp, int ifwp, int ofhp, int ofwp, int ifh, int ifw,
+#define T_ofm_tile 4
+#define T_ifm_tile 1
+#define T_oj 4
+#define T_oi 28
+/* With libxsmm parameters*/
+void padded_conv_fp_stride_1_tiled(int nImg, int nIfm, int nOfm, int ifhp, int ifwp, int ofhp, int ofwp, int ifh, int ifw,
 	int ofh, int ofw, int pad_h, int pad_w, int pad_h_in, int pad_w_in, int pad_h_out,
 	int pad_w_out, int kh, int kw, int stride_h, int stride_w,
 	const float pad_gemm_input[nImg][nIfm / 16][ifhp + 2 * pad_h][ifwp + 2 * pad_w][16], float output[nImg][nOfm / 16][ofhp][ofwp][16], const float filter[nOfm / 16][nIfm / 16][kh][kw][16][16])
 {
+	// printf("LIBXMM version\n");
+	/*
+	FWD params...
+Fwd_ofw_rb = 28
+Fwd_ofh_rb = 1
+Pack input = 0
+Block oj = 4
+Loop order = 1
+Blocksifm_blocking = 1
+Block fwd ofm = 4
+Block fwd ifm = 1
+Avoid rim fmas = 0
+Ofm parallelization = 0
+Shuffle filter accesses = 0
+Avoid acc load = 1
+Fwd GEMM flags = 640
+	*/
+
 	/* loop counters */
 	int img, ofm_tile, ofm, ifm_tile, ifm, oj, oi, ij, ii, kj, ki;
 	int t_ofm_tile, t_ifm_tile, t_oj, t_oi;
 
 #pragma scop
 	for (img = 0; img < nImg; ++img) {
-		// Tile loops
 		for (t_ofm_tile = 0; t_ofm_tile < nOfm / 16; t_ofm_tile += T_ofm_tile) {
-			for (t_ifm_tile = 0; t_ifm_tile < nIfm / 16; t_ifm_tile += T_ifm_tile) {
-				for (t_oj = 0; t_oj < ofh; t_oj += T_oj) {
+			for (t_oj = 0; t_oj < ofh; t_oj += T_oj) {
+				for (oj = t_oj; oj < min(ofh, t_oj + T_oj); ++oj) {
 					for (t_oi = 0; t_oi < ofw; t_oi += T_oi) {
+						for (ofm_tile = t_ofm_tile; ofm_tile < min(nOfm / 16, t_ofm_tile + T_ofm_tile); ++ofm_tile) {
+							for (t_ifm_tile = 0; t_ifm_tile < nIfm / 16; t_ifm_tile += T_ifm_tile) {
+								for (ifm_tile = t_ifm_tile; ifm_tile < min(nIfm / 16, t_ifm_tile + T_ifm_tile); ++ifm_tile) {
+									for (kj = 0; kj < kh; ++kj) {
+										for (ki = 0; ki < kw; ++ki) {
 
-						// Intra-tile loops
-						for (ofm_tile = t_ofm_tile;
-							ofm_tile < min(t_ofm_tile + T_ofm_tile, nOfm / 16); ++ofm_tile) {
-							for (ifm_tile = t_ifm_tile;
-								ifm_tile < min(t_ifm_tile + T_ifm_tile, nIfm / 16); ++ifm_tile) {
-								for (oj = t_oj; oj < min(t_oj + T_oj, ofh); ++oj) {
-									for (oi = t_oi; oi < min(t_oi + T_oi, ofw); ++oi) {
-
-										for (kj = 0; kj < kh; ++kj) {
-											for (ki = 0; ki < kw; ++ki) {
-
-												// GEMM
+											/*
+											// GEMM
+											// min(ofw, t_oi + T_oi) is simplified to t_oi + T_oi because T_oi divides ofw.
+											for (oi = t_oi; oi < t_oi + T_oi; ++oi) {
 												for (ofm = 0; ofm < 16; ++ofm) {
 													for (ifm = 0; ifm < 16; ++ifm) {
 														output[img][ofm_tile][oj][oi][ofm] +=
@@ -110,6 +122,12 @@ void padded_conv_fp_stride_1_tiled_core(int nImg, int nIfm, int nOfm, int ifhp, 
 													}
 												}
 											}
+											*/
+
+											fwd_gemm(&filter[ofm_tile][ifm_tile][kj][ki][0][0],
+												&pad_gemm_input[img][ifm_tile][oj + kj][t_oi + ki][0],
+												&output[img][ofm_tile][oj][t_oi][0]);
+
 										}
 									}
 								}
@@ -359,7 +377,7 @@ void padded_conv_fp_stride_1(
 	}
 	else if (version == 1) {
 		// printf("padded_conv_fp_stride_1_tiled_core\n");
-		padded_conv_fp_stride_1_tiled_core(nImg, nIfm, nOfm, ifhp, ifwp, ofhp, ofwp, ifh, ifw,
+		padded_conv_fp_stride_1_tiled(nImg, nIfm, nOfm, ifhp, ifwp, ofhp, ofwp, ifh, ifw,
 			ofh, ofw, pad_h, pad_w, pad_h_in, pad_w_in, pad_h_out,
 			pad_w_out, kh, kw, stride_h, stride_w, pad_gemm_input, output, filter);
 	}
@@ -504,9 +522,9 @@ void compare_buf(float* ref, float* test, long size, correctness_t* norms)
 		}
 #endif
 
-	}
+		}
 	norms->l2_rel_err = sqrt(norms->l2_rel_err);
-}
+		}
 
 int main(int argc, char **argv) {
 	int ifhp, ifwp, ofhp, ofwp, ofh, ofw;
@@ -603,7 +621,20 @@ int main(int argc, char **argv) {
 #if defined(USE_LIBXSMM)
 	int ldx;
 	ldx = 16;
-	fwd_gemm = libxsmm_smmdispatch(16, ofwp, 16, NULL, &ldx, NULL, NULL, NULL, NULL, NULL);
+
+	if (version == 1) {
+		// LIBXSMM tiled
+		if (ofwp % T_oi != 0) {
+			printf("The tiling factor %d for oi loop should divide ofwp = %d\n. Exiting\n", T_oi, ofwp);
+			return -1;
+		}
+
+		fwd_gemm = libxsmm_smmdispatch(16, T_oi, 16, NULL, &ldx, NULL, NULL, NULL, NULL, NULL);
+	}
+	else {
+		fwd_gemm = libxsmm_smmdispatch(16, ofwp, 16, NULL, &ldx, NULL, NULL, NULL, NULL, NULL);
+	}
+
 #endif
 
 	printf("Allocating data\n");
@@ -619,6 +650,7 @@ int main(int argc, char **argv) {
 
 	printf("Initializing data\n");
 	/* initialize data */
+	srand48(100);
 	init_buf(&naive_input[0][0][0][0], nImg*nIfm*ifhp*ifwp);
 	init_buf(&gemm_input[0][0][0][0], nImg*nIfm*ifhp*ifwp);
 	init_buf(&naive_filter[0][0][0][0], nOfm*nIfm*kh*kw);
