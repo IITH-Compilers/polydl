7b72d91edcdf5ffbebe1cd1696dfff2d565917fe
sanket <sanketst@gmail.com>
2019-07-16 14:48:33 +0530

    Moving print functions to Utility.cpp

--- data_reuse_analyzer/Main.cpp
+++ data_reuse_analyzer/Main.cpp
@@ -12,11 +12,13 @@
 #include <fstream>
 #include <ConfigProcessor.hpp>
 #include <OptionsProcessor.hpp>
+#include <Utility.hpp>
 using namespace std;
 
 
 #define IGNORE_WS_SIZE_ONE 1
 #define DEBUG 1
+#define INTERACTIVE 0
 
 /* Function header declarations begin */
 struct WorkingSetSize {
@@ -28,7 +30,6 @@ struct WorkingSetSize {
 };
 
 typedef struct WorkingSetSize WorkingSetSize;
-
 
 struct ProgramCharacteristics {
 	int L1Fit; // #working sets that fit in L1 cache
@@ -53,23 +54,14 @@ struct ArgComputeWorkingSetSizesForDependence {
 typedef struct ArgComputeWorkingSetSizesForDependence  ArgComputeWorkingSetSizesForDependence;
 
 void ComputeDataReuseWorkingSets(const char *fileName);
-void PrintScop(isl_ctx* ctx, struct pet_scop *scop);
-void PrintExpressions(isl_printer *printer, pet_expr *expr);
 pet_scop* ParseScop(isl_ctx* ctx, const char *fileName);
 isl_union_map* ComputeDataDependences(isl_ctx* ctx, pet_scop* scop);
 isl_stat ComputeWorkingSetSizesForDependence(isl_map* dep, void *user);
-void PrintUnionFlow(isl_union_flow* flow);
 vector<WorkingSetSize*>* ComputeWorkingSetSizesForDependences(
 	isl_union_map *dependences,
 	pet_scop *scop);
-void PrintUnionMap(isl_union_map* map);
-void PrintMap(isl_map* map);
 isl_stat ComputeWorkingSetSizesForDependenceBasicMap(isl_basic_map* dep,
 	void *user);
-void PrintBasicMap(isl_basic_map* map);
-void PrintUnionSet(isl_union_set* set);
-void PrintSet(isl_set* set);
-void PrintUnionPwQpolynomial(isl_union_pw_qpolynomial* poly);
 isl_union_pw_qpolynomial* ComputeDataSetSize(isl_basic_set* sourceDomain,
 	isl_set* source, isl_set* target, pet_scop* scop);
 isl_union_pw_qpolynomial* ComputeDataSetSize(isl_union_set* WS,
@@ -78,7 +70,6 @@ void FreeWorkingSetSizes(vector<WorkingSetSize*>* workingSetSizes);
 void PrintWorkingSetSizes(vector<WorkingSetSize*>* workingSetSizes);
 void SimplifyWorkingSetSizes(vector<WorkingSetSize*>* workingSetSizes,
 	string fileName);
-void PrintSpace(isl_space* space);
 string SimplifyUnionPwQpolynomial(isl_union_pw_qpolynomial* size,
 	unordered_map<string, int>* paramValues);
 unordered_map<string, int>* GetParameterValues(vector<WorkingSetSize*>* workingSetSizes);
@@ -87,7 +78,13 @@ long ExtractIntegerFromUnionPwQpolynomial(isl_union_pw_qpolynomial* poly);
 string GetParameterValuesString(unordered_map<string, int>* paramValues);
 isl_union_map* ComputeDataDependences(isl_union_map *source,
 	isl_union_map *target, isl_schedule* schedule);
+void OrchestrateDataReuseComputation(int argc, char **argv);
 /* Function header declarations end */
+
+int main(int argc, char **argv) {
+	OrchestrateDataReuseComputation(argc, argv);
+	return 0;
+}
 
 void OrchestrateDataReuseComputation(int argc, char **argv) {
 	string fileName = "../apps/padded_conv_fp_stride_1_libxsmm_core2.c";
@@ -108,10 +105,6 @@ void OrchestrateDataReuseComputation(int argc, char **argv) {
 	delete userInput;
 }
 
-int main(int argc, char **argv) {
-	OrchestrateDataReuseComputation(argc, argv);
-	return 0;
-}
 
 void ComputeDataReuseWorkingSets(const char *fileName) {
 	isl_ctx* ctx = isl_ctx_alloc_with_pet_options();
@@ -649,205 +642,11 @@ isl_union_map* ComputeDataDependences(isl_union_map *source,
 	return may_dependences;
 }
 
-void PrintUnionPwQpolynomial(isl_union_pw_qpolynomial* poly) {
-	isl_printer *printer = isl_printer_to_file(
-		isl_union_pw_qpolynomial_get_ctx(poly), stdout);
-	printer = isl_printer_set_output_format(printer, ISL_FORMAT_ISL);
-	isl_printer_print_union_pw_qpolynomial(printer, poly);
-	cout << endl;
-
-	//printer = isl_printer_set_output_format(printer, ISL_FORMAT_C);
-	//isl_printer_print_union_pw_qpolynomial(printer, poly);
-	isl_printer_free(printer);
-}
-
-void PrintBasicMap(isl_basic_map* map) {
-	isl_printer *printer = isl_printer_to_file(
-		isl_basic_map_get_ctx(map), stdout);
-	printer = isl_printer_set_output_format(printer, ISL_FORMAT_ISL);
-	isl_printer_print_basic_map(printer, map);
-	cout << endl;
-	isl_printer_free(printer);
-}
-
-void PrintMap(isl_map* map) {
-	isl_printer *printer = isl_printer_to_file(
-		isl_map_get_ctx(map), stdout);
-	printer = isl_printer_set_output_format(printer, ISL_FORMAT_ISL);
-	isl_printer_print_map(printer, map);
-	cout << endl;
-	isl_printer_free(printer);
-}
-
-void PrintSpace(isl_space* space) {
-	isl_printer *printer = isl_printer_to_file(
-		isl_space_get_ctx(space), stdout);
-	printer = isl_printer_set_output_format(printer, ISL_FORMAT_ISL);
-	isl_printer_print_space(printer, space);
-	cout << endl;
-	isl_printer_free(printer);
-}
-
-void PrintUnionMap(isl_union_map* map) {
-	isl_printer *printer = isl_printer_to_file(
-		isl_union_map_get_ctx(map), stdout);
-	printer = isl_printer_set_output_format(printer, ISL_FORMAT_ISL);
-	isl_printer_print_union_map(printer, map);
-	cout << endl;
-	isl_printer_free(printer);
-}
-
-void PrintSet(isl_set* set) {
-	isl_printer *printer = isl_printer_to_file(
-		isl_set_get_ctx(set), stdout);
-	printer = isl_printer_set_output_format(printer, ISL_FORMAT_ISL);
-	isl_printer_print_set(printer, set);
-	cout << endl;
-	isl_printer_free(printer);
-}
-
-void PrintUnionSet(isl_union_set* set) {
-	isl_printer *printer = isl_printer_to_file(
-		isl_union_set_get_ctx(set), stdout);
-	printer = isl_printer_set_output_format(printer, ISL_FORMAT_ISL);
-	isl_printer_print_union_set(printer, set);
-	cout << endl;
-	isl_printer_free(printer);
-}
-
-void PrintUnionFlow(isl_union_flow* flow) {
-	isl_ctx* ctx = isl_union_flow_get_ctx(flow);
-	isl_printer *printer = isl_printer_to_file(ctx, stdout);
-	printer = isl_printer_set_output_format(printer, ISL_FORMAT_ISL);
-	printer = isl_printer_print_union_flow(printer, flow);
-	cout << endl;
-	isl_printer_free(printer);
-}
 
 pet_scop* ParseScop(isl_ctx* ctx, const char *fileName) {
 	pet_options_set_autodetect(ctx, 0);
 	pet_scop *scop = pet_scop_extract_from_C_source(ctx, fileName, NULL);
 	return scop;
-}
-
-void PrintScop(isl_ctx* ctx, struct pet_scop *scop) {
-	isl_printer *printer = isl_printer_to_file(ctx, stdout);
-	printer = isl_printer_set_output_format(printer, ISL_FORMAT_ISL);
-
-	cout << "pet_scop_get_context:" << endl;
-	isl_printer_print_set(printer, pet_scop_get_context(scop));
-	cout << endl;
-
-	cout << "pet_scop_get_schedule:" << endl;
-	isl_printer_print_schedule(printer, pet_scop_get_schedule(scop));
-	cout << endl;
-
-	cout << "pet_scop_get_instance_set:" << endl;
-	isl_printer_print_union_set(printer, pet_scop_get_instance_set(scop));
-	cout << endl;
-
-	cout << "pet_scop_get_may_reads:" << endl;
-	isl_printer_print_union_map(printer, pet_scop_get_may_reads(scop));
-	cout << endl;
-	return;
-
-	// Print Context
-	isl_set *context = scop->context;
-	cout << "Context:" << endl;
-	isl_printer_print_set(printer, context);
-
-	// Print context_value
-	cout << "context_value:" << endl;
-	isl_printer_print_set(printer, scop->context_value);
-
-	// Print context_value
-	// cout << "schedule:" << endl;
-	// isl_printer_print_schedule(printer, scop->schedule);
-
-	cout << endl << "#pet_types:" << scop->n_type << endl;
-	for (int i = 0; i < scop->n_type; i++) {
-		cout << "name: " << scop->types[i]->name << endl;
-		cout << "definition: " << scop->types[i]->definition << endl;
-	}
-
-	cout << "scop->n_array: " << scop->n_array << endl;
-	for (int i = 0; i < scop->n_array; i++) {
-
-		if (scop->arrays[i]->context) {
-			cout << "scop->arrays[i]->context: " << endl;
-			isl_printer_print_set(printer, scop->arrays[i]->context);
-		}
-
-		if (scop->arrays[i]->extent) {
-			cout << "scop->arrays[i]->extent: " << endl;
-			isl_printer_print_set(printer, scop->arrays[i]->extent);
-		}
-
-		if (scop->arrays[i]->value_bounds) {
-			cout << "scop->arrays[i]->value_bounds: " << endl;
-			isl_printer_print_set(printer, scop->arrays[i]->value_bounds);
-		}
-	}
-
-	cout << "n_stmt: " << scop->n_stmt << endl;
-	for (int i = 0; i < scop->n_stmt; i++) {
-		cout << "Printing " << i << " th statement" << endl;
-		cout << "scop->stmts[i]->domain: " << endl;
-		isl_printer_print_set(printer, scop->stmts[i]->domain);
-
-		cout << "Body: " << endl;
-		pet_tree_dump(scop->stmts[i]->body);
-		cout << endl;
-		if (pet_tree_is_loop(scop->stmts[i]->body)) {
-			cout << "It is a LOOP" << endl;
-		}
-		else {
-			cout << "It is NOT a loop" << endl;
-		}
-
-		cout << "scop->stmts[i]->n_arg: " << scop->stmts[i]->n_arg << endl;
-		for (int j = 0; j < scop->stmts[i]->n_arg; i++) {
-			pet_expr_dump(scop->stmts[i]->args[i]);
-			cout << endl;
-		}
-
-		if (pet_tree_get_type(scop->stmts[i]->body) == pet_tree_expr) {
-			PrintExpressions(printer,
-				pet_tree_expr_get_expr(scop->stmts[i]->body));
-		}
-	}
-
-	isl_printer_free(printer);
-}
-
-void PrintExpressions(isl_printer *printer, pet_expr *expr) {
-	if (expr) {
-		if (pet_expr_get_type(expr) == pet_expr_access) {
-			cout << "Access relation: " << endl;
-			pet_expr_dump(expr);
-			cout << endl << "pet_expr_get_n_arg(expr): "
-				<< pet_expr_get_n_arg(expr) << endl;
-			if (/* pet_expr_is_affine(expr) && */
-				pet_expr_access_is_read(expr)) {
-				cout << "Encountered a READ expression: " << endl;
-				isl_printer_print_union_map(printer,
-					pet_expr_access_get_may_read(expr));
-			}
-			else if (/* pet_expr_is_affine(expr) && */
-				pet_expr_access_is_write(expr)) {
-				cout << "Encountered a WRITE expression: " << endl;
-				isl_printer_print_union_map(printer,
-					pet_expr_access_get_may_write(expr));
-			}
-
-			cout << endl;
-		}
-		else {
-			for (int i = 0; i < pet_expr_get_n_arg(expr); i++) {
-				PrintExpressions(printer, pet_expr_get_arg(expr, i));
-			}
-		}
-	}
 }
 
 
