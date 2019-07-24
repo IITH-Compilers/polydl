b522f36d4b5082a2f0161c9bb33eca8d37203a55
Gaganiith <cs19mtech01003@iith.ac.in>
2019-07-24 14:58:59 +0530

    Added benchmarking with MobileNet and AlexNet

--- mkl_build/MKLbenchmarks.sh
+++ mkl_build/MKLbenchmarks.sh
@@ -1,7 +1,7 @@
 rm -rf perf.csv
 cd mkl-dnn/build/
-cp ../../run_bench.sh ./tests/benchdnn/
+cp ../../run_bench_all.sh ./tests/benchdnn/
 cd ./tests/benchdnn
-sh run_bench.sh
+sh run_bench_all.sh
 cp ./perf.csv ../../../../
 echo "success"
