fa172b391e87715cdc88d3f95a3a4811d54ceeb1
Sanket Tavarageri <sanket.tavarageri@intel.com>
2019-08-30 03:53:18 -0700

    Adding non-unit convolution support

--- apps/padded_conv_fp_tiled_loop_order_1.c
+++ apps/padded_conv_fp_tiled_loop_order_1.c
@@ -1,3 +1,4 @@
+
 #define min(X, Y) (((X) < (Y)) ? (X) : (Y))
 #define max(X, Y) (((X) > (Y)) ? (X) : (Y))
 
@@ -17,9 +18,9 @@
 #define T_oi 28
 #endif // !T_oi
 
-
 #define GEMM_BLOCK 64
-void padded_conv_fp_stride_1_tiled_loop_order_1(int nImg, int nIfm, int nOfm, int ifhp, int ifwp, int ofhp, int ofwp, int ifh, int ifw,
+
+void padded_conv_fp_tiled_loop_order_1(int nImg, int nIfm, int nOfm, int ifhp, int ifwp, int ofhp, int ofwp, int ifh, int ifw,
 	int ofh, int ofw, int pad_h, int pad_w, int pad_h_in, int pad_w_in, int pad_h_out,
 	int pad_w_out, int kh, int kw, int stride_h, int stride_w,
 	const float pad_gemm_input[nImg][nIfm / GEMM_BLOCK][ifhp + 2 * pad_h][ifwp + 2 * pad_w][GEMM_BLOCK], float output[nImg][nOfm / GEMM_BLOCK][ofhp][ofwp][GEMM_BLOCK], const float filter[nOfm / GEMM_BLOCK][nIfm / GEMM_BLOCK][kh][kw][GEMM_BLOCK][GEMM_BLOCK], int iters)
@@ -47,11 +48,12 @@ Fwd GEMM flags = 640
 	int t_ofm_tile, t_ifm_tile, t_oj, t_oi;
 
 #pragma scop
-#pragma omp parallel for private(img, t_ofm_tile, t_oj, oj, t_oi, ofm_tile, t_ifm_tile, ifm_tile, kj, ki)
+#pragma omp parallel for private(img, t_ofm_tile, t_oj, oj, t_oi, ofm_tile, t_ifm_tile, ifm_tile, kj, ki, ii, ij)
 		for (img = 0; img < nImg; ++img) {
 			for (t_ofm_tile = 0; t_ofm_tile < nOfm / GEMM_BLOCK; t_ofm_tile += T_ofm_tile) {
 				for (t_oj = 0; t_oj < ofh; t_oj += T_oj) {
 					for (oj = t_oj; oj < min(ofh, t_oj + T_oj); ++oj) {
+						ij = oj * stride_h;
 						for (t_oi = 0; t_oi < ofw; t_oi += T_oi) {
 							for (ofm_tile = t_ofm_tile; ofm_tile < min(nOfm / GEMM_BLOCK, t_ofm_tile + T_ofm_tile); ++ofm_tile) {
 								for (t_ifm_tile = 0; t_ifm_tile < nIfm / GEMM_BLOCK; t_ifm_tile += T_ifm_tile) {
@@ -63,19 +65,21 @@ Fwd GEMM flags = 640
 												// GEMM
 												// min(ofw, t_oi + T_oi) is simplified to t_oi + T_oi because T_oi divides ofw.
 												for (oi = t_oi; oi < t_oi + T_oi; ++oi) {
+													ii = oi * stride_w;
 													for (ofm = 0; ofm < GEMM_BLOCK; ++ofm) {
 														for (ifm = 0; ifm < GEMM_BLOCK; ++ifm) {
 															output[img][ofm_tile][oj][oi][ofm] +=
-																filter[ofm_tile][ifm_tile][kj][ki][ifm][ofm] * pad_gemm_input[img][ifm_tile][oj + kj][oi + ki][ifm];
+																filter[ofm_tile][ifm_tile][kj][ki][ifm][ofm] * pad_gemm_input[img][ifm_tile][ij + kj][ii + ki][ifm];
 														}
 													}
 												}
 												
 
 /*												fwd_gemm(&filter[ofm_tile][ifm_tile][kj][ki][0][0],
-													&pad_gemm_input[img][ifm_tile][oj + kj][t_oi + ki][0],
+													&pad_gemm_input[img][ifm_tile][ij + kj][t_oi + ki][0],
 													&output[img][ofm_tile][oj][t_oi][0]);
 */
+
 											}
 										}
 									}
