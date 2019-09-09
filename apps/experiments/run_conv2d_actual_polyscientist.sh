6a8dbaaee3755d6aa6a25e9b3dd8e1a40102d67f
Sanket Tavarageri <sanket.tavarageri@intel.com>
2019-09-09 01:34:39 -0700

    Adding stride pragma to the C files

--- apps/experiments/run_conv2d_actual_polyscientist.sh
+++ apps/experiments/run_conv2d_actual_polyscientist.sh
@@ -34,19 +34,21 @@ mkdir ${PERF_DIR}
 mkdir ${TEMP}
 for config in "$config1" "$config2" "$config3" "$config4" "$config5" "$config6" "$config7" "$config8" "$config9" "$config10" "$config11" "$config12" "$config13" "$config14" "$config15" "$config16" "$config17" "$config18" "$config19" #FIXME 
 do
+       params=( ${config} )
+       ofw=${params[1]}
+       ofh=${params[2]}
+       nIfm=${params[3]}
+       nOfm=${params[4]}
+       stride=${params[9]}
+
 	for images in 1 28
 	do
 	        CONFIG_OUT=${PERF_DIR}/${config_num}_${images}_${OUT}
 	        rm ${CONFIG_OUT}
 
 		export OMP_NUM_THREADS=${images}
-		for version in 0 2 3 4 5 1
+		for version in 2 3 4 5 #0 1
 		do
-			params=( ${config} )
-			ofw=${params[1]}
-			ofh=${params[2]}
-			nIfm=${params[3]}
-			nOfm=${params[4]}
 			#We will first do an actual run
 			if [ $version -eq 0 -o $version -eq 1 ]
 			then
@@ -84,7 +86,9 @@ do
 					fi
 
 					rm tile_sizes.c
-					echo "#define T_oi ${T_oi}" > tile_sizes.c
+					echo "#define STRIDE_H ${stride}" >> tile_sizes.c
+					echo "#define STRIDE_W ${stride}" >> tile_sizes.c
+					echo "#define T_oi ${T_oi}" >> tile_sizes.c
 					echo "#define T_oj ${T_oj}" >> tile_sizes.c
 					echo "#define T_ifm_tile ${T_ifm_tile}" >> tile_sizes.c
 					echo "#define T_ofm_tile ${T_ofm_tile}" >> tile_sizes.c
@@ -110,24 +114,26 @@ do
 				(cd .. && make clean && make) 	
 				GFLOPS=`../conv2d $config ${images} ${version} ${check_correctness} |  grep GFLOPS |  cut -d= -f2`
                                 rm ${TEMP}/temp.c
+                                echo "#define STRIDE_H ${stride}" >> ${TEMP}/temp.c
+                                echo "#define STRIDE_W ${stride}" >> ${TEMP}/temp.c
                                 if [ $version -eq 2 ]
                                 then
-                                cp ../padded_conv_fp_libxsmm_core.c ${TEMP}/temp.c
+                                cat ../padded_conv_fp_libxsmm_core.c >> ${TEMP}/temp.c
                                 fi
 
                                 if [ $version -eq 3 ]
                                 then
-                                cp ../padded_conv_fp_libxsmm_core2.c ${TEMP}/temp.c
+                                cat ../padded_conv_fp_libxsmm_core2.c >> ${TEMP}/temp.c
                                 fi
 
                                 if [ $version -eq 4 ]
                                 then
-                                cp ../padded_conv_fp_libxsmm_core3.c ${TEMP}/temp.c
+                                cat ../padded_conv_fp_libxsmm_core3.c >> ${TEMP}/temp.c
                                 fi
 
                                 if [ $version -eq 5 ]
                                 then
-                                cp ../padded_conv_fp_libxsmm_core4.c ${TEMP}/temp.c
+                                cat ../padded_conv_fp_libxsmm_core4.c >> ${TEMP}/temp.c
                                 fi
 
                                 config_file=${config_num}_${images}_conv_config.txt
