1b30599076e468af6c731a0431099d9c0b91ff19
Sanket Tavarageri <sanket.tavarageri@intel.com>
2019-10-25 20:44:04 -0700

    Adding mkl dnn related files

--- mkl_build/run_simple_mkl.sh
+++ mkl_build/run_simple_mkl.sh
@@ -1,5 +1,6 @@
-OUT=perf.csv
+OUT=mkl_perf.csv
 #rm ${OUT}
+BENCHDNN=/nfs_home/stavarag/work/polyscientist/mkl_build/mkl-dnn/build/tests/benchdnn/benchdnn
 
 #Default values.
 iters=1
@@ -31,12 +32,18 @@ config_num=${11}
 oh=$(((ifh + 2*pad_h - kh)/stride + 1))
 ow=$(((ifw + 2*pad_w - kw)/stride + 1))
 
+for mb in 1 28
+do
+
+export KMP_AFFINITY=granularity=fine,compact,1,28
+export OMP_NUM_THREADS=$mb
 
 config="g1mb${mb}ic${nIfm}ih${ifh}iw${ifw}oc${nOfm}oh${56}ow${56}kh${kh}kw${kw}sh${stride}sw${stride}ph${pad_h}pw${pad_w}n"
 
 #echo -n $config, >> ${OUT}
-GFLOPS=`mkl-dnn/build/tests/benchdnn/benchdnn --conv --mode=p --dir=FWD_D --cfg=f32 $config"resnet_50:conv1" | grep "nresnet_50:conv1"| cut -d"," -f10`
-echo -n $GFLOPS >> ${OUT}
-echo "" >> ${OUT}
+GFLOPS=`$BENCHDNN --conv --mode=p --dir=FWD_D --cfg=f32 $config"resnet_50:conv1" | grep "nresnet_50:conv1"| cut -d"," -f10`
+echo -n $GFLOPS >> ${mb}_${OUT}
+echo "" >> ${mb}_${OUT}
 
-echo >> ${OUT}
+
+done
