9520a1c3f8de4abd66f6e076d86534b5c6e1ee58
Gaganiith <cs19mtech01003@iith.ac.in>
2020-03-03 22:54:10 +0530

    fixed run_conv2d.sh in experiment folder with polyscientist executable

--- clean_apps/version/padded_conv_fp_libxsmm_core2.c
+++ clean_apps/version/padded_conv_fp_libxsmm_core2.c
@@ -11,6 +11,11 @@
 #define GEMM_BLOCK 64
 #endif // !GEMM_BLOCK
 
+#ifndef LIBXSMM_H
+#include <libxsmm.h>
+#endif
+extern libxsmm_smmfunction fwd_gemm;
+
 void padded_conv_fp_libxsmm_core2_fn(int nImg, int nIfm, int nOfm, int ifhp, int ifwp, int ofhp, int ofwp, int ifh, int ifw,
 	int ofh, int ofw, int pad_h, int pad_w, int pad_h_in, int pad_w_in, int pad_h_out,
 	int pad_w_out, int kh, int kw, int stride_h, int stride_w,
@@ -56,7 +61,7 @@ void padded_conv_fp_libxsmm_core2_fn(int nImg, int nIfm, int nOfm, int ifhp, int
 }
 
 
-void padded_conv_fp_libxsmm_core2_gemm(int nImg, int nIfm, int nOfm, int ifhp, int ifwp, int ofhp, int ofwp, int ifh, int ifw,
+void padded_conv_fp_high_perfomrance(int nImg, int nIfm, int nOfm, int ifhp, int ifwp, int ofhp, int ofwp, int ifh, int ifw,
 	int ofh, int ofw, int pad_h, int pad_w, int pad_h_in, int pad_w_in, int pad_h_out,
 	int pad_w_out, int kh, int kw, int stride_h, int stride_w,
 	const float pad_gemm_input[nImg][nIfm / GEMM_BLOCK][ifhp + 2 * pad_h][ifwp + 2 * pad_w][GEMM_BLOCK], float output[nImg][nOfm / GEMM_BLOCK][ofhp][ofwp][GEMM_BLOCK], const float filter[nOfm / GEMM_BLOCK][nIfm / GEMM_BLOCK][kh][kw][GEMM_BLOCK][GEMM_BLOCK], int iters)
@@ -100,6 +105,7 @@ void padded_conv_fp_libxsmm_core2_gemm(int nImg, int nIfm, int nOfm, int ifhp, i
 	}
 
 }
+
 
 static inline void padded_conv_fp_libxsmm_core2_vanilla_gemm(int nImg, int nIfm, int nOfm, int ifhp, int ifwp, int ofhp, int ofwp, int ifh, int ifw,
 	int ofh, int ofw, int pad_h, int pad_w, int pad_h_in, int pad_w_in, int pad_h_out,
