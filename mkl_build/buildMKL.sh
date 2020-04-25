e54aaf16ad1570494b326456ade83e9a6ef41604
Sanket Tavarageri <sanket.tavarageri@intel.com>
2020-04-25 01:29:14 -0700

    Updating the version of MKL-DNN

--- mkl_build/buildMKL.sh
+++ mkl_build/buildMKL.sh
@@ -1,7 +1,7 @@
 rm -rf mkl-dnn
 git clone https://github.com/intel/mkl-dnn.git
 cd mkl-dnn
-git checkout tags/v1.0.4
+git checkout tags/v1.4
 mkdir -p build install
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=../install/ ..
