b99144036de9c8a166c30b3833f0b527543c0655
Sanket Tavarageri <sanket.tavarageri@intel.com>
2020-04-27 22:35:08 -0700

    Updating the script

--- apps/apps_matmul/experiments/run_matmul.sh
+++ apps/apps_matmul/experiments/run_matmul.sh
@@ -1,6 +1,10 @@
 
+set -x
 export KMP_AFFINITY=granularity=fine,compact,1,28
 export LD_LIBRARY_PATH=/nfs_home/stavarag/work/software/barvinok/barvinok-0.41.2_install/lib:/nfs_home/stavarag/work/software/barvinok/isl_install/lib:$LD_LIBRARY_PATH
+
+export LD_LIBRARY_PATH=/nfs_home/stavarag/work/software/barvinok/barvinok-0.41.2_install/lib:/nfs_home/stavarag/work/software/barvinok/isl_install/lib:$LD_LIBRARY_PATH
+
 
 OUT=poly_perf.csv
 
@@ -23,10 +27,11 @@ M1_Tile=$8
 N1_Tile=$9
 K1_Tile=${10}
 file=${11}
+PARALLEL_LOOP=${12}
 
 echo iters=$iters M1=$M1 N1=$N1 K1=$K1 M2_Tile=$M2_Tile N2_Tile=$N2_Tile K2_Tile=$K2_Tile M1_Tile=$M1_Tile N1_Tile=$N1_Tile K1_Tile=$K1_Tile file=$file
 
-config=${iters}_${M1}_${N1}_${K1}_${M2_Tile}_${M2_Tile}_${N2_Tile}_${K2_Tile}_${M1_Tile}_${N1_Tile}_${K1_Tile}
+config=${iters}_${M1}_${N1}_${K1}_${M2_Tile}_${M2_Tile}_${N2_Tile}_${K2_Tile}_${M1_Tile}_${N1_Tile}_${K1_Tile}_${PARALLEL_LOOP}
 echo config: $config
 
 CONFIG_OUT=${PERF_DIR}/${file}_${OUT}
@@ -63,5 +68,14 @@ ERROR=`cat run_output | grep "inf-norm of comp. abs. error" | cut -d: -f 2`
 echo GFLOPS: $GFLOPS
 echo ERROR: $ERROR
 
-echo  "${config},${GFLOPS}"  >> ${CONFIG_OUT}
+
+CACHE_CONFIG='L1 32768 L2 1048576 L3 40370176'
+DATATYPESIZE=4
+NUM_PROCS=28
+output_file=${EXPERIMENTS_DIR}/${WORKFILE}_ws_stats.csv
+../../../data_reuse_analyzer/polyscientist --input $EXPERIMENTS_DIR/$WORKFILE --parameters 'dummy : 50 ' --cachesizes "${CACHE_CONFIG}" --datatypesize $DATATYPESIZE --minout --parallel_loops ${PARALLEL_LOOP} --numprocs ${NUM_PROCS} --sharedcaches L3 
+
+{ echo -n "${config},${GFLOPS}," ;  cat ${output_file} ; }  >> ${CONFIG_OUT}
 echo  "${config},${ERROR}" >> ${META_CONFIG_OUT}
+
+
