16d3ebd11e1c1af87cd046c26c2c786ca7ec3aa9
Sanket Tavarageri <sanket.tavarageri@intel.com>
2020-08-10 00:27:11 -0700

    Updates to GEMM recognizer

--- MLIR/miscellaneous/polydl_rt.c
+++ MLIR/miscellaneous/polydl_rt.c
@@ -16,7 +16,7 @@ void print_f32_polydl(
 
 }
 
-void polydl_lib_matmul_f32(float* A,
+void polydl_lib_matmul_f32_2(float* A,
 	long long int M1, long long int K1,
 	long long int A_stride1, long long int A_stride2,
 	float* B,
@@ -33,6 +33,26 @@ void polydl_lib_matmul_f32(float* A,
 			for (k = 0; k < K1; k++) {
 				C[i*C_stride1 + j * C_stride2] +=
 					A[i*A_stride1 + k * A_stride2] * B[k*B_stride1 + j * B_stride2];
+			}
+		}
+	}
+}
+
+void polydl_lib_matmul_f32(
+	long long int M, long long int N, long long int K,
+	long long int A_stride, long long int B_stride, long long int C_stride,
+	float *A, float *B, float *C) {
+
+	printf("In polydl_lib_matmul_f32\n");
+	printf("M = %ld, N = %ld, K = %ld, A_stride = %ld, B_stride = %ld, C_stride = %ld\n",
+		M, N, K, A_stride, B_stride, C_stride);
+
+	int i, j, k;
+	for (i = 0; i < M; i++) {
+		for (j = 0; j < N; j++) {
+			for (k = 0; k < K; k++) {
+				C[i*C_stride + j] +=
+					A[i*A_stride + k] * B[k*B_stride + j];
 			}
 		}
 	}
